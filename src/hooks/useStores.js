460a6b0f433623814e5cce93540f45b4de59ab68
Mia Watanabe <mia.noriko.watanabe@gmail.com>
2023-01-01 07:52:29 -0800

    Changed context folder name from context to contexts

--- src/hooks/useStores.js
+++ src/hooks/useStores.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { StoresContext } from '../context/StoresContextProvider';
+import { StoresContext } from '../contexts/StoresContextProvider';
 
 const useStores = () => useContext(StoresContext);
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import './index.css';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './context/AuthProvider';
-import StoresContextProvider from './context/StoresContextProvider';
+import { AuthProvider } from './contexts/AuthProvider';
+import StoresContextProvider from './contexts/StoresContextProvider';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
